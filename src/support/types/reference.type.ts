ea4710851bd9da1259443e53ca227eabb215661d
Juan Carlos <jcharlylc@gmail.com>
2024-04-24 22:25:24 -0400

    Provider upsert types

--- src/support/types/reference.type.ts
+++ src/support/types/reference.type.ts
@@ -17,3 +17,30 @@ export interface IMedicalOfficeUpdateArgs {
 	name?:			string
 	belongingId?:	number
 }
+
+export interface IProviderCreateArgs {
+	vendorCode:		string
+	businessName:	string
+	nit?:			string
+	address?:		string
+	phone?:			string
+
+	belongingId:	number
+
+	medicalGroups: Array<{
+		medicalGroupId:	number
+		specialties: Array<{
+			medicalSpecialtyId: number
+			subspecialties: Array<number>
+		}>
+	}>
+}
+
+export interface IProviderUpdateArgs {
+	businessName?:	string
+	nit?:			string
+	address?:		string
+	phone?:			string
+
+	belongingId?:	number
+}
