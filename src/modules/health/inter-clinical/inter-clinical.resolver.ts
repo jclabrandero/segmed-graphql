063f9f9f2931c80c5ea5761d35ab8f7265289415
Juan Carlos <jcharlylc@gmail.com>
2024-04-29 21:10:32 -0400

    Module: Interclinical

---
+++ src/modules/health/inter-clinical/inter-clinical.resolver.ts
@@ -0,0 +1,153 @@
+
+import { ClinicCare } from '@prisma/client'
+
+import { Relation, Resolver } from '../../../support/classes'
+import { IContext, IInterclinicalCreateArgs, IInterclinicalUpdateArgs } from '../../../support/types'
+import { Status, SubscriptionEvent } from '../../../support/constants'
+import { now, withAuditForCreate, withAuditForDelete, withAuditForUpdate } from '../../../support/functions'
+import { MedicalGroupResolver } from '../../catalog'
+
+
+export class InterclinicalResolver extends Resolver {
+
+	constructor() {
+		super(SubscriptionEvent.ClinicCare)
+	}
+
+	async findOne(_, { id }: { id: number }, { db }: IContext) {
+		const record = await db.interclinical.findUnique({
+			where: {
+				id,
+				NOT: { status: Status.Removed }
+			},
+			include: {
+				medicalGroup: true,
+				provider: true,
+				specialties: {
+					include: {
+						medicalSpecialty: true,
+						subspecialties: {
+							include: {
+								medicalSubspecialty: true
+							}
+						}
+					}
+				},
+				files: {
+					where: {
+						status: Status.Active
+					},
+					include: {
+						file: true
+					}
+				}
+			}
+		})
+
+		return {
+			...record,
+			medicalGroup: {
+				...record.medicalGroup,
+				specialties: record.specialties.map(specialty => ({
+					...specialty.medicalSpecialty,
+					subspecialties: specialty.subspecialties.map(subspecialty => subspecialty.medicalSubspecialty)
+				}))
+			},
+			files: record.files.map(ref => ref.file)
+		}
+	}
+
+	async create(_, { data }: { data: IInterclinicalCreateArgs }, { db, pubsub, user }: IContext): Promise<ClinicCare> {
+		const { UPDATED, UPSERTED } = SubscriptionEvent.ClinicCare
+		const { clinicCareId, medicalGroupId, specialties, ...payload } = data
+		const groups = await MedicalGroupResolver.index({ db } as IContext)
+		const record = await db.clinicCare.update({
+			where: { id: clinicCareId },
+			data: {
+				interclinicals: {
+					create: [{
+						...withAuditForCreate(user, payload),
+						medicalGroupId,
+						driftDate: now().local,
+						specialties: {
+							create: specialties.map(({ medicalSpecialtyId, subspecialties }) => ({
+								...withAuditForCreate(user, { medicalSpecialtyId, specialtyName: groups[medicalGroupId].specialties[medicalSpecialtyId].name }),
+								subspecialties: {
+									create: subspecialties.map(medicalSubspecialtyId => 
+										withAuditForCreate(user, {
+											medicalSubspecialtyId,
+											subspecialtyName: groups[medicalGroupId].specialties[medicalSpecialtyId].subspecialties[medicalSubspecialtyId].name
+										})
+									)
+								}
+							}))
+						}
+					}]
+				}
+			}
+		})
+		super.publish({
+			pubsub,
+			events: [UPDATED, UPSERTED],
+			dataset: [{ clinicCareUpdated: record }, { clinicCareUpserted: record }]
+		})
+		return record
+	}
+
+	async update(source, { id, data }: { id: number, data: IInterclinicalUpdateArgs }, { db, pubsub, user }: IContext): Promise<ClinicCare> {
+		const { UPDATED, UPSERTED } = SubscriptionEvent.ClinicCare
+		const { clinicCareId, files, ...payload } = data
+		const md5ToId = async () => {
+			const result = []
+			for (const md5 of files) {
+				result.push((await db.fileUpload.findUnique({ where: { md5 }})).id)
+			}
+			return result
+		}
+		const record = await db.clinicCare.update({
+			where: { id: clinicCareId },
+			data: {
+				interclinicals: {
+					update: {
+						where: { id },
+						data: {
+							...withAuditForUpdate(user, payload),
+							files: files ? await Relation.upsert({
+								model: db.interclinicalFileUpload, where: { interclinicalId: id }, dataset: await md5ToId(), field: 'fileUploadId', user
+							}) : undefined
+						}
+					}
+				}
+			}
+		})
+		super.publish({
+			pubsub,
+			events: [UPDATED, UPSERTED],
+			dataset: [{ clinicCareUpdated: record }, { clinicCareUpserted: record }]
+		})
+		return record
+	}
+
+	async delete(source, { id, data }: { id: number, data: IInterclinicalUpdateArgs }, { db, pubsub, user }: IContext): Promise<ClinicCare> {
+		const { UPDATED, UPSERTED } = SubscriptionEvent.ClinicCare
+		const { clinicCareId } = data
+		const record = await db.clinicCare.update({
+			where: { id: clinicCareId },
+			data: {
+				interclinicals: {
+					update: {
+						where: { id },
+						data: withAuditForDelete(user)
+					}
+				}
+			}
+		})
+		super.publish({
+			pubsub,
+			events: [UPDATED, UPSERTED],
+			dataset: [{ clinicCareUpdated: record }, { clinicCareUpserted: record }]
+		})
+		return record
+	}
+
+}
