4e20f944cb6cc3f265cba7db113ec92f82f1147d
Juan Carlos <jcharlylc@gmail.com>
2024-04-29 20:58:56 -0400

    Provider resolver exposition

--- src/modules/reference/index.ts
+++ src/modules/reference/index.ts
@@ -1,8 +1,10 @@
 
 import { BelongingResolver } from './belonging/belonging.resolver'
 import { MedicalOfficeResolver } from './medical-office/medical-office.resolver'
+import { ProviderResolver } from './provider/provider.resolver'
 
 export {
 	BelongingResolver,
-	MedicalOfficeResolver
+	MedicalOfficeResolver,
+	ProviderResolver
 }
