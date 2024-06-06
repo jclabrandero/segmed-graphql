f7b34d981ff6f0bd71ea7a8962180031ed7d2123
Juan Carlos <jcharlylc@gmail.com>
2024-06-06 00:00:55 -0400

    Fix: Relation upsert function

--- src/support/classes/relation.class.ts
+++ src/support/classes/relation.class.ts
@@ -7,7 +7,7 @@ export class Relation {
 
 	public static async upsert({ model, where, dataset, field, user }) {
 		const records = await model.findMany({ where })
-		const list = dataset.map(id => JSON.parse(`{"${field}":${id}}`))
+		const list = dataset.length && (typeof dataset[0] == 'number') ? dataset.map(id => JSON.parse(`{"${field}":${id}}`)) : dataset
 		const useFind = e => r => e[field] == r[field]
 		const update = []
 
@@ -15,24 +15,40 @@ export class Relation {
 			const { id, status } = record
 			const found = list.find(useFind(record))
 
-			switch (status) {
-			case Status.Active:
-			case Status.Idle:
-			case Status.Pending:
-				if (!found) update.push({ where: { id }, data: withAuditForDelete(user) })
-				break
-			case Status.Removed:
-				if (found) update.push({ where: { id }, data: withAuditForUpdate(user, { status: Status.Active }) })
-				break
+			if (found) {
+				const { __update, ...remaining } = found
+				switch (status) {
+				case Status.Active:
+				case Status.Idle:
+				case Status.Pending:
+					if (__update) update.push({ where: { id }, data: withAuditForUpdate(user, remaining) })
+					break
+				case Status.Removed:
+					update.push({ where: { id }, data: withAuditForUpdate(user, { ...remaining, status: Status.Active }) })
+					break
+				}
+			} else {
+				switch (status) {
+				case Status.Active:
+				case Status.Idle:
+				case Status.Pending:
+					update.push({ where: { id }, data: withAuditForDelete(user) })
+					break
+				}
 			}
 		})
 
 		const create = list.reduce(
-			(prev, item) =>	records.find(useFind(item)) ? prev : [ ...prev, ...[withAuditForCreate(user, item)] ],
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			(prev, { __update, ...remaining }) =>	records.find(useFind(remaining)) ? prev : [ ...prev, ...[withAuditForCreate(user, remaining)] ],
 			[]
 		)
 
-		return { update, create }
+		if (Boolean(update.length)) {
+			return Boolean(create.length) ? { update, create } : { update } 
+		} else {
+			return Boolean(create.length) ? { create } : {} 
+		}
 	}
 
 }
