196c4727eaeb2b069af589b3f46cdb77687293ad
reycaredwin <reycar.edwin@gmail.com>
2025-02-25 18:54:56 -0400

    migration update tables

--- prisma/migrations/20250125185129_006/migration.sql
+++ prisma/migrations/20250125185129_006/migration.sql
@@ -2,8 +2,13 @@ BEGIN TRY
 
 BEGIN TRAN;
 
--- AlterTable
-ALTER TABLE [dbo].[Arrival] ADD [invoiceTotalRefPrice] DECIMAL(32,16) NOT NULL CONSTRAINT [Arrival_invoiceTotalRefPrice_df] DEFAULT 0;
+-- Verificar si la columna existe antes de agregarla
+IF NOT EXISTS (SELECT * FROM sys.columns 
+               WHERE Name = N'invoiceTotalRefPrice' 
+               AND Object_ID = Object_ID(N'dbo.Arrival'))
+BEGIN
+    ALTER TABLE [dbo].[Arrival] ADD [invoiceTotalRefPrice] DECIMAL(32,16) NOT NULL CONSTRAINT [Arrival_invoiceTotalRefPrice_df] DEFAULT 0;
+END
 
 COMMIT TRAN;
 
--- prisma/migrations/20250125201128_007/migration.sql
+++ prisma/migrations/20250125201128_007/migration.sql
@@ -2,11 +2,34 @@ BEGIN TRY
 
 BEGIN TRAN;
 
--- AlterTable
-ALTER TABLE [dbo].[Arrival] ADD [approvalDate] DATETIME2,
-[approvalState] INT NOT NULL CONSTRAINT [Arrival_approvalState_df] DEFAULT 0,
-[approvalUserName] NVARCHAR(1000),
-[closed] BIT NOT NULL CONSTRAINT [Arrival_closed_df] DEFAULT 0;
+-- Verificar si las columnas existen antes de agregarlas
+IF NOT EXISTS (SELECT * FROM sys.columns 
+               WHERE Name = N'approvalDate' 
+               AND Object_ID = Object_ID(N'dbo.Arrival'))
+BEGIN
+    ALTER TABLE [dbo].[Arrival] ADD [approvalDate] DATETIME2;
+END
+
+IF NOT EXISTS (SELECT * FROM sys.columns 
+               WHERE Name = N'approvalState' 
+               AND Object_ID = Object_ID(N'dbo.Arrival'))
+BEGIN
+    ALTER TABLE [dbo].[Arrival] ADD [approvalState] INT NOT NULL CONSTRAINT [Arrival_approvalState_df] DEFAULT 0;
+END
+
+IF NOT EXISTS (SELECT * FROM sys.columns 
+               WHERE Name = N'approvalUserName' 
+               AND Object_ID = Object_ID(N'dbo.Arrival'))
+BEGIN
+    ALTER TABLE [dbo].[Arrival] ADD [approvalUserName] NVARCHAR(1000);
+END
+
+IF NOT EXISTS (SELECT * FROM sys.columns 
+               WHERE Name = N'closed' 
+               AND Object_ID = Object_ID(N'dbo.Arrival'))
+BEGIN
+    ALTER TABLE [dbo].[Arrival] ADD [closed] BIT NOT NULL CONSTRAINT [Arrival_closed_df] DEFAULT 0;
+END
 
 COMMIT TRAN;
 
