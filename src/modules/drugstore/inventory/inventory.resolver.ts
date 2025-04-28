aa4f83f37bed7165f9fd5c6d365283ffd37dac25
reycaredwin <reycar.edwin@gmail.com>
2025-04-27 22:21:37 -0400

    inventory report

--- src/modules/drugstore/inventory/inventory.resolver.ts
+++ src/modules/drugstore/inventory/inventory.resolver.ts
@@ -1,6 +1,6 @@
 
 import { Inventory } from '@prisma/client'
-
+import path from 'path'
 import { Resolver, Template } from '../../../support/classes'
 import { Status, SubscriptionEvent } from '../../../support/constants'
 import { IContext, IInventoryPrintReportArgs } from '../../../support/types'
@@ -48,58 +48,103 @@ export class InventoryResolver extends Resolver {
 		})
 
 		const template = new  Template
-		const tabledata: Array<Array<{ text: string, style?: string }>> = [
+		const logoPath = path.join(__dirname, '..', 'src', 'assets', 'logo_seguro.jpg') // Ruta del logo
+		const tabledata: Array<Array<{ text: string, style?: string, alignment?: string, fontSize?: string}>> = [
 			[
 				{ text: 'Código', style: 'tableHeader' },
 				{ text: 'Nombre Medicamento', style: 'tableHeader' },
 				{ text: 'Unidad', style: 'tableHeader' },
-				{ text: 'Concentración', style: 'tableHeader' }
+				{ text: 'Concentración', style: 'tableHeader' },
+            	{ text: 'Cantidad en Stock', style: 'tableHeader' },
+            	{ text: 'Cantidad Contada', style: 'tableHeader' },
+            	{ text: 'Diferencia', style: 'tableHeader' }
 			],
 		]
-		for (const { medication } of data) {
+		
+		for (const { medication, stock } of data) {
 			tabledata.push([
 				{ text: medication.code, style: 'tableRow' },
 				{ text: medication.name, style: 'tableRow' },
 				{ text: medication.unit.name, style: 'tableRow' },
-				{ text: medication.concentration, style: 'tableRow' }
+				{ text: medication.concentration, style: 'tableRow' },
+            	{ text: stock.toString(), style: 'tableRow', alignment: 'center', fontSize: '10' }, // Cantidad en Stock
+            	{ text: '', style: 'tableRow' }, // Cantidad Contada se llenará manualmente
+            	{ text: '', style: 'tableRow' }  // Diferencia se calculará manualmente
 			])
 		}
 		const buffer = await template.build({
 			pageSize: 'LETTER',
-			pageMargins: [30, 30, 30, 40],
+			pageMargins: [30, 70, 30, 40],
 			pageOrientation: 'landscape',
-			header: (currentPage) => {
+			header: () => {
 				return [
 					{
-						columns: [
-							{
-								text: 'CIERRE MENSUAL SALDOS - MEDICAMENTOS'
-							},
-							{
-								text: `Pagina ${currentPage}`
-							}
-						]
-					},
+						table: {
+							widths: ['*', '*', '*'],
+							body: [
+								[
+									{ image: logoPath, width: 150, alignment: 'left' },
+									{ text: 'Inventario Físico de Farmacia', alignment: 'center', bold: true, fontSize: 16, margin: [0, 30, 0, 0] },
+									{ text: `Fecha: ${new Date().toLocaleDateString()}`, alignment: 'right', margin: [0, 30, 0, 0]}
+								]
+							],
+						},
+						margin: [30, 10, 30, 10],
+						layout: 'noBorders'
+					}
 				]
 			},
+			footer: (currentPage, pageCount) => {
+				return {
+					columns: [
+						{ text: '', alignment: 'left' },
+						{ text: '', alignment: 'center' },
+						{ text: `Página ${currentPage} de ${pageCount}`, alignment: 'right', margin: [0, 0, 30, 0] }
+					]
+				}
+			},
+
 			content: [
 				{
 					table: {
-						widths: '*',
+						headerRows: 1, // Repetir encabezados en cada página
+						dontBreakRows: true,
+						widths: [50, 150, 155, 155, 50, 50, 60], // Ajuste de anchos de columnas
 						body: tabledata
-					}
+					},
+					margin: [0, 20, 0, 0] // Ajuste de margen para evitar superposición
+				},
+				{
+					text: 'Fecha de Conteo: ........................................',
+					margin: [0, 50, 0, 0]
+				},
+				{
+					text: 'Responsable: ........................................',
+					margin: [0, 50, 0, 20]
 				}
 			],
 			styles: {
 				tableHeader: {
 					bold: true,
-					fontSize: 11,
-					color: 'black'
+					fontSize: 11, // Ajuste de tamaño de fuente
+					color: 'black',
+					alignment: 'center',
+					margin: [0, 5, 0, 0],
+					fillColor: '#D3D3D3', // Color plomo oscuro
 				},
 				tableRow: {
-					fontSize: 10,
-				}
-			}
+					fontSize: 9, // Ajuste de tamaño de fuente
+					//margin: [0, 5, 0, 5],
+					Height: 1000, // Ajuste de altura de la celda
+					//font: 'Helvetica'
+				},
+				tableRowCenter: {
+					fontSize: 9, // Ajuste de tamaño de fuente
+					alignment: 'center',
+					margin: [0, 5, 0, 5],
+					lineHeight: 1.2,
+					//font: 'Helvetica' // Ajuste de altura de la celda
+				}}
 		})
 		return {
 			info: {
